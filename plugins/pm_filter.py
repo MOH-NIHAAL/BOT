7c9e6fd8274a91f6f7f661483d97d6771da32e74
MOH-NIHAAL <104961284+MOH-NIHAAL@users.noreply.github.com>
2022-05-26 08:27:48 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1559,7 +1559,7 @@ async def advantage_spell_chok(msg):
         ]] 
         imdb=await get_poster(search)
         if imdb and imdb.get('poster'):
-        k=await msg.reply_photo(photo=imdb.get('poster'), caption=script.IMDB_MOVIE_2.format(mention=message.from_user.mention, query=search, title=imdb.get('title'), genres=imdb('genres'), year=imdb.get('year'), rating=imdb.get('rating'), url=imdb['url'], short=imdb['short_info']), reply_markup=reply_markup)     
+        k=await msg.reply_photo(photo=imdb.get('poster'), caption=script.IMDB_MOVIE_2.format(mention=message.from_user.mention, query=search, title=imdb.get('title'), year=imdb.get('year'), rating=imdb.get('rating'), url=imdb['url'], short=imdb['short_info']), reply_markup=reply_markup)     
         await asyncio.sleep(40)
         await k.delete()
         await msg.delete()
@@ -1597,7 +1597,7 @@ async def advantage_spell_chok(msg):
         ]]
         imdb=await get_poster(search)
         if imdb and imdb.get('poster'):
-        k=await msg.reply_photo(photo=imdb.get('poster'), caption=script.IMDB_MOVIE_2.format(mention=message.from_user.mention, query=search, title=imdb.get('title'), genres=imdb('genres'), year=imdb.get('year'), rating=imdb.get('rating'), url=imdb['url'], short=imdb['short_info']), reply_markup=reply_markup)     
+        k=await msg.reply_photo(photo=imdb.get('poster'), caption=script.IMDB_MOVIE_2.format(mention=message.from_user.mention, query=search, title=imdb.get('title'), year=imdb.get('year'), rating=imdb.get('rating'), url=imdb['url'], short=imdb['short_info']), reply_markup=reply_markup)     
         await asyncio.sleep(40)
         await k.delete()
         await msg.delete()
@@ -1611,7 +1611,7 @@ async def advantage_spell_chok(msg):
     ]]
     imdb=await get_poster(search)
     if imdb and imdb.get('poster'):
-    k=await msg.reply_photo(photo=imdb.get('poster'), caption=script.IMDB_MOVIE_2.format(mention=message.from_user.mention, query=search, title=imdb.get('title'), genres=imdb('genres'), year=imdb.get('year'), rating=imdb.get('rating'), url=imdb['url'], short=imdb['short_info']), reply_markup=reply_markup)     
+    k=await msg.reply_photo(photo=imdb.get('poster'), caption=script.IMDB_MOVIE_2.format(mention=message.from_user.mention, query=search, title=imdb.get('title'), year=imdb.get('year'), rating=imdb.get('rating'), url=imdb['url'], short=imdb['short_info']), reply_markup=reply_markup)     
     await asyncio.sleep(40)
     await k.delete()
     await msg.delete()
